42931f5e88a6f0fcb236525e976dea2cd08afd9c
cybermias <34351049+cybermias@users.noreply.github.com>
2018-02-15 18:48:18 +0200

    Create postInstall.ps1

---
+++ postInstall.ps1
@@ -0,0 +1,110 @@
+## If needed, use this to "force" clean win10 after deployment
+
+$tweaks = @(
+	### Require administrator privileges ###
+	"RequireAdmin",
+
+	### Privacy Settings ###
+	"UninstallOneDrive",             "DisableLockScreenRS1",
+	"DisableLockScreen",             # "EnableWiFiSense",
+	"DisableCtrlAltDelLogin"
+)
+
+# Relaunch the script with administrator privileges
+Function RequireAdmin {
+	If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+		Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $PSCommandArgs" -WorkingDirectory $pwd -Verb RunAs
+		Exit
+	}
+}
+
+# Disable Lock screen
+Function DisableLockScreen {
+	Write-Host "Disabling Lock screen..."
+	If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization")) {
+		New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization" | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization" -Name "NoLockScreen" -Type DWord -Value 1
+}
+
+# Disable Lock screen (Anniversary Update workaround) - Applicable to 1607 or newer
+Function DisableLockScreenRS1 {
+	Write-Host "Disabling Lock screen using scheduler workaround..."
+	$service = New-Object -com Schedule.Service
+	$service.Connect()
+	$task = $service.NewTask(0)
+	$task.Settings.DisallowStartIfOnBatteries = $false
+	$trigger = $task.Triggers.Create(9)
+	$trigger = $task.Triggers.Create(11)
+	$trigger.StateChange = 8
+	$action = $task.Actions.Create(0)
+	$action.Path = "reg.exe"
+	$action.Arguments = "add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\SessionData /t REG_DWORD /v AllowLockScreen /d 0 /f"
+	$service.GetFolder("\").RegisterTaskDefinition("Disable LockScreen", $task, 6, "NT AUTHORITY\SYSTEM", $null, 4) | Out-Null
+}
+
+Function SetVisualFXPerformance {
+	Write-Host "Adjusting visual effects for performance..."
+	Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "DragFullWindows" -Type String -Value 0
+	Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "MenuShowDelay" -Type String -Value 0
+	Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "UserPreferencesMask" -Type Binary -Value ([byte[]](0x90,0x12,0x03,0x80,0x10,0x00,0x00,0x00))
+	Set-ItemProperty -Path "HKCU:\Control Panel\Desktop\WindowMetrics" -Name "MinAnimate" -Type String -Value 0
+	Set-ItemProperty -Path "HKCU:\Control Panel\Keyboard" -Name "KeyboardDelay" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ListviewAlphaSelect" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ListviewShadow" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAnimations" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" -Name "VisualFXSetting" -Type DWord -Value 3
+	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\DWM" -Name "EnableAeroPeek" -Type DWord -Value 0
+}
+
+Function UninstallOneDrive {
+	Write-Host "Uninstalling OneDrive..."
+	Stop-Process -Name OneDrive -ErrorAction SilentlyContinue
+	Start-Sleep -s 3
+	$onedrive = "$env:SYSTEMROOT\SysWOW64\OneDriveSetup.exe"
+	If (!(Test-Path $onedrive)) {
+		$onedrive = "$env:SYSTEMROOT\System32\OneDriveSetup.exe"
+	}
+	Start-Process $onedrive "/uninstall" -NoNewWindow -Wait
+	Start-Sleep -s 3
+	Stop-Process -Name explorer -ErrorAction SilentlyContinue
+	Start-Sleep -s 3
+	Remove-Item "$env:USERPROFILE\OneDrive" -Force -Recurse -ErrorAction SilentlyContinue
+	Remove-Item "$env:LOCALAPPDATA\Microsoft\OneDrive" -Force -Recurse -ErrorAction SilentlyContinue
+	Remove-Item "$env:PROGRAMDATA\Microsoft OneDrive" -Force -Recurse -ErrorAction SilentlyContinue
+	Remove-Item "$env:SYSTEMDRIVE\OneDriveTemp" -Force -Recurse -ErrorAction SilentlyContinue
+	If (!(Test-Path "HKCR:")) {
+		New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT | Out-Null
+	}
+	Remove-Item -Path "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Recurse -ErrorAction SilentlyContinue
+	Remove-Item -Path "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Recurse -ErrorAction SilentlyContinue
+}
+
+# Disable Ctrl+Alt+Del requirement before login
+Function DisableCtrlAltDelLogin {
+	Write-Host "Disabling Ctrl+Alt+Del requirement before login..."
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DisableCAD" -Type DWord -Value 1
+}
+
+##########
+# Parse parameters and apply tweaks
+##########
+
+# Normalize path to preset file
+$preset = ""
+$PSCommandArgs = $args
+If ($args -And $args[0].ToLower() -eq "-preset") {
+	$preset = Resolve-Path $($args | Select-Object -Skip 1)
+	$PSCommandArgs = "-preset `"$preset`""
+}
+
+# Load function names from command line arguments or a preset file
+If ($args) {
+	$tweaks = $args
+	If ($preset) {
+		$tweaks = Get-Content $preset -ErrorAction Stop | ForEach { $_.Trim() } | Where { $_ -ne "" -and $_[0] -ne "#" }
+	}
+}
+
+# Call the desired tweak functions
+$tweaks | ForEach { Invoke-Expression $_ }
